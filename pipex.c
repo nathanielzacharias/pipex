d0b4dcfff2e9e711a08cd791c6774013f28de750
Nathaniel Zacharias <nathanielzacharias@gmail.com>
2024-02-16 14:21:42 +0800

    feat: added main with argc == 5 & argv

--- pipex.c
+++ pipex.c
@@ -6,8 +6,21 @@
 /*   By: nzachari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 18:39:44 by nzachari          #+#    #+#             */
-/*   Updated: 2023/11/10 18:40:01 by nzachari         ###   ########.fr       */
+/*   Updated: 2024/02/16 14:19:59 by nzachari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//new file
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+int	main(int argc, char *av[])
+{
+	if (argc != 5)
+	{
+		printf("must have 5 args");
+		return (0);
+	}
+	printf("small win");
+}
