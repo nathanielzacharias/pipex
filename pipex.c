9e8b17e51286b99d0177a6d1c4924131a81472bf
Nathaniel Zacharias <nathanielzacharias@gmail.com>
2024-02-19 18:35:50 +0800

    feat: added libft and func pathvar global var environ

--- pipex.c
+++ pipex.c
@@ -15,59 +15,78 @@
 # include <stdio.h>
 # include <fcntl.h>
 # include <errno.h>
+# include "libft/libft.h"
 
 extern const char **environ;
 
-
-//parse_pipex(pid, )
-char	*binpath;
-char	**cmd_flag;
-int		fd;
-
-cmd_flag = ft_split(cmd, ' ');
-
-
-//else pipe_pipex(pid, pipefd, file, cmd)
-
-
-
-int	main(int ac, char *av[])
+void pathvar()
 {
-	if (ac != 5)
-		return (errno = EINVAL, perror("Program requires 5 args"), 1);
-//	else return (printf("win"), 0);
-
-	int	pipefd[2];
-	pipe(pipefd);
-
-	int rpipe = pipefd[0];
-	int wpipe = pipefd[1];
-	printf("rpipe is:", rpipe);
-	printf("wpipe is:", wpipe);
-
-	int pid;
-	pid = fork();
-	if (pid < 0) return (errno = ESRCH, perror("pid < 0"), 1);
-
-	char *infile = av[1];
-	char *cmd1 = av[2];
-	char *cmd2 = av[3];
-	char *outfile = av[4];
-
-	if (pid == 0)
+	ssize_t	i;
+	i = -1;
+	while(environ[++i])
 	{
-		close(rpipe);
-		//parse_pipex(infile, cmd1, rpipe, pid);
-		return (0);
+		if(!ft_strncmp((environ[i]), "PATH=", 5))
+			printf("%s", environ[i]);
 	}
 
-	//search in $PATH /bin directory for cmd1
-	//check access to 
-
-	else
-	{
-		wait();
-		close(wpipe);
-		//parse_pipex(outfile, cmd2, wpipe, pid);
-	}
 }
+
+int main(void)
+{
+	pathvar();
+	return 0;
+}
+
+
+// //parse_pipex(pid, )
+// char	*binpath;
+// char	**cmd_flag;
+// int		fd;
+
+// cmd_flag = ft_split(cmd, ' ');
+
+
+// //else pipe_pipex(pid, pipefd, file, cmd)
+
+
+
+// int	main(int ac, char *av[])
+// {
+// 	if (ac != 5)
+// 		return (errno = EINVAL, perror("Program requires 5 args"), 1);
+// //	else return (printf("win"), 0);
+
+// 	int	pipefd[2];
+// 	pipe(pipefd);
+
+// 	int rpipe = pipefd[0];
+// 	int wpipe = pipefd[1];
+// 	printf("rpipe is:", rpipe);
+// 	printf("wpipe is:", wpipe);
+
+// 	int pid;
+// 	pid = fork();
+// 	if (pid < 0) return (errno = ESRCH, perror("pid < 0"), 1);
+
+// 	char *infile = av[1];
+// 	char *cmd1 = av[2];
+// 	char *cmd2 = av[3];
+// 	char *outfile = av[4];
+
+// 	if (pid == 0)
+// 	{
+// 		close(rpipe);
+// 		//parse_pipex(infile, cmd1, rpipe, pid);
+// 		return (0);
+// 	}
+
+// 	//search in $PATH /bin directory for cmd1
+// 	//check access to 
+
+// 	else
+// 	{
+// 		wait();
+// 		close(wpipe);
+// 		//parse_pipex(outfile, cmd2, wpipe, pid);
+// 	}
+// }
