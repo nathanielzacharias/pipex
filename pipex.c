bcf3d8e9ae4fc20a1d1cedfae7f9972d66dc387e
Nathaniel Zacharias <nathanielzacharias@gmail.com>
2024-03-18 14:06:37 +0800

    fix: used two fork() to return control to main

--- pipex.c
+++ pipex.c
@@ -58,6 +58,7 @@ char	*access_ok(char *cmd)
 		{	
 			tmp = pathwcmd;
 			free(indv_paths);
+			// free(pathwcmd);
 			return (tmp);
 		}
 		free(pathwcmd);
@@ -75,34 +76,34 @@ int	 parse_pipex(char *cmd, int pid, int fd, int pipe_end)
 	
 	cmd_args = ft_split(cmd, ' ');
 	binpath = access_ok(cmd_args[0]);
+	// printf("pid is : %d \n", pid);
 	if (!binpath)
 		return (errno = EACCES, perror("access() denied"), 1);
+	// if (pid ==1) printf("binpath is: %s\n", binpath );
+	
 	if (pid == 0)
 	{
 		checkdupchild = dup2(pipe_end, STDOUT_FILENO);
 		if (checkdupchild < 0) return(perror("dup2() failed in child"), 1);
 		execve(binpath, cmd_args, (char *const *)environ);
-		perror("In child: ");
+		// perror("In child: ");
 	}
 	else
 	{
 		checkdupparent = dup2(pipe_end, STDIN_FILENO);
+		// close(pipe_end);
 		if (checkdupparent < 0 ) return(perror("dup2() failed in parent"), 1);
 		execve(binpath, cmd_args, (char *const *)environ);
-		perror("In parent: ");
+		perror("In parent second child: ");
 	}
 	free(cmd_args);
 	return(0);
 }
 
-int hasErrors(int ac, int pipefd[])
-{
-	if (ac != 5)
-		return(errno = EINVAL, perror("Program requires 5 args"), 1);
-	pipe_success = pipe(pipefd);
-	if (pipe_success < 0)
-		return (errno = EPIPE, perror("pipe_success < 0"), 1);
-}
+// int hasErrors(int ac, char *av[], int pipefd[])
+// {
+
+// }
 
 int	main(int ac, char *av[])
 {
@@ -110,27 +111,52 @@ int	main(int ac, char *av[])
 	int out_fd;
 	int	pipefd[2];
 	int	pid;
+	int pipe_success;
 
-	if hasErrors()
-
+	// if hasErrors()
+	if (ac != 5)
+		return(errno = EINVAL, perror("Program requires 5 args"), 1);
+	pipe_success = pipe(pipefd);
+	if (pipe_success < 0)
+		return (errno = EPIPE, perror("pipe_success < 0"), 1);
 	in_fd = open(av[1], O_RDONLY);
 	out_fd = open(av[ac -1], O_TRUNC | O_CREAT | O_WRONLY, 0644);
 	if (in_fd < 0 || out_fd < 0)
 		return(errno = EBADFD, perror("open() returns -1 for in_fd or out_fd"), 1);
-	dup2(in_fd, STDIN_FILENO);
-	dup2(out_fd, STDOUT_FILENO);
+	
+	printf("in main before fork\n");
+
 	pid = fork();
 	if (pid < 0)
 		return(errno = ESRCH, perror("pid < 0"), 1);
+
 	if (pid == 0)
 	{
+		printf("in first child\n");
+		dup2(in_fd, STDIN_FILENO);
+		close(in_fd);
 		close(pipefd[0]);
-		parse_pipex(av[2], pid, STDIN_FILENO, pipefd[1]);
+		parse_pipex(av[2], 0, STDIN_FILENO, pipefd[1]);
 	}
+
 	else
 	{
-		waitpid(-1, NULL, 0);
-		close(pipefd[1]);
-		parse_pipex(av[3], pid, STDOUT_FILENO, pipefd[0]);
+		waitpid(pid, NULL, 0);
+		printf("in main parent\n");fflush(NULL);
+		pid = fork();
+		if (pid < 0)
+			return(errno = ESRCH, perror("pid < 0"), 1);
+		if (pid == 0)
+		{
+			printf("in main parent after fork in child\n");fflush(NULL);
+			dup2(out_fd, STDOUT_FILENO);
+			close(out_fd);
+			close(pipefd[1]);
+			parse_pipex(av[3], 1, STDOUT_FILENO, pipefd[0]);
+		}
 	}
+
+	// waitpid(pid, NULL, 0);
+	// printf("in main parent after fork after child\n");fflush(NULL);
+	return(0);
 }
