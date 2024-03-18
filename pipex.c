39a7a4275c8507e279287fc1647ab0e941b0bd52
Nathaniel Zacharias <nathanielzacharias@gmail.com>
2024-03-18 14:33:29 +0800

    feat: added struct to allow refactor of code

--- pipex.c
+++ pipex.c
@@ -20,6 +20,13 @@
 
 extern const char **environ;
 
+typedef	struct s_fdstruct
+{
+	int in_fd;
+	int out_fd;
+	int	pipefd[2];
+}	t_fdstruct;
+
 const char *get_pathvar()
 {
 	ssize_t	i;
@@ -91,37 +98,38 @@ int	 parse_pipex(char *cmd, int pid, int pipe_end)
 	return(0);
 }
 
-// int hasErrors(int ac, char *av[], int pipefd[])
-// {
-
-// }
-
-int	main(int ac, char *av[])
+int hasErrors(int ac, char *av[], t_fdstruct *fds)
 {
-	int in_fd;
-	int out_fd;
-	int	pipefd[2];
-	int	pid;
 	int pipe_success;
 
 	if (ac != 5)
 		return(errno = EINVAL, perror("Program requires 5 args"), 1);
-	pipe_success = pipe(pipefd);
+	pipe_success = pipe(fds->pipefd);
 	if (pipe_success < 0)
 		return (errno = EPIPE, perror("pipe_success < 0"), 1);
-	in_fd = open(av[1], O_RDONLY);
-	out_fd = open(av[ac -1], O_TRUNC | O_CREAT | O_WRONLY, 0644);
-	if (in_fd < 0 || out_fd < 0)
+	fds->in_fd = open(av[1], O_RDONLY);
+	fds->out_fd = open(av[ac -1], O_TRUNC | O_CREAT | O_WRONLY, 0644);
+	if (fds->in_fd < 0 || fds->out_fd < 0)
 		return(errno = EBADFD, perror("open() returns -1 for in_fd or out_fd"), 1);
+	return(0);
+}
+
+int	main(int ac, char *av[])
+{
+	int	pid;
+	t_fdstruct *fds;
+
+	if (hasErrors(ac, av, fds))
+		return(1);
 	pid = fork();
 	if (pid < 0)
 		return(errno = ESRCH, perror("pid < 0"), 1);
 	if (pid == 0)
 	{
-		dup2(in_fd, STDIN_FILENO);
-		close(in_fd);
-		close(pipefd[0]);
-		parse_pipex(av[2], 0, pipefd[1]);
+		dup2(fds->in_fd, STDIN_FILENO);
+		close(fds->in_fd);
+		close(fds->pipefd[0]);
+		parse_pipex(av[2], 0, fds->pipefd[1]);
 	}
 	else
 	{
@@ -131,10 +139,10 @@ int	main(int ac, char *av[])
 			return(errno = ESRCH, perror("pid < 0"), 1);
 		if (pid == 0)
 		{
-			dup2(out_fd, STDOUT_FILENO);
-			close(out_fd);
-			close(pipefd[1]);
-			parse_pipex(av[3], 1, pipefd[0]);
+			dup2(fds->out_fd, STDOUT_FILENO);
+			close(fds->out_fd);
+			close(fds->pipefd[1]);
+			parse_pipex(av[3], 1, fds->pipefd[0]);
 		}
 	}
 	return(0);
